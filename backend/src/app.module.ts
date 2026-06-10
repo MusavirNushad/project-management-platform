9c504964cf90e736b2b205b95b6980604453caaf
Musavir Nushad <musavirnushad@gmail.com>
2026-06-10 13:25:12 +0500

    project management platform v-1.09 --- register the middleware in app.module.ts.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,20 @@
 // src/app.module.ts
 
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { RequestLoggingMiddleware } from './shared/infrastructure/middlewares/request-logging.middleware';
+
+
 import { IdentityModule } from './modules/identity/identity.module';
+import { AccessControlModule } from './modules/access-control/access-control.module';
 import { WorkspaceModule } from './modules/workspace/workspace.module';
 import { ProjectModule } from './modules/project/project.module';
 import { TaskModule } from './modules/task/task.module';
 import { SprintModule } from './modules/sprint/sprint.module';
 import { WorklogModule } from './modules/worklog/worklog.module';
 import { ReportModule } from './modules/report/report.module';
+
 
 @Module({
   imports: [
@@ -18,6 +23,7 @@ import { ReportModule } from './modules/report/report.module';
     }),
 
     IdentityModule,
+    AccessControlModule,
     WorkspaceModule,
     ProjectModule,
     TaskModule,
@@ -27,4 +33,8 @@ import { ReportModule } from './modules/report/report.module';
 
   ],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  }
+}
